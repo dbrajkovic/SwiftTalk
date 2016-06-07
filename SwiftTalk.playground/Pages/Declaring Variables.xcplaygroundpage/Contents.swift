f84fdbc5bc0a96daec1f6a0ed4d18fab8f1cf286
Dan Brajkovic <dbrajkovic@mac.com>
2016-06-07 12:03:09 -0500

    Minor changes

--- SwiftTalk.playground/Pages/Declaring Variables.xcplaygroundpage/Contents.swift
+++ SwiftTalk.playground/Pages/Declaring Variables.xcplaygroundpage/Contents.swift
@@ -3,12 +3,15 @@
 //: ## Declaring Variables
 let hoursPerDay = 24
 var hoursSlept = 8
+
+let 🍎 = "Apple"
 //: ### Mutable types
 let firstName = "Bob"
 var name = "Bob"
 name += " Jones"
 
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
 var registeredUsers = ["Dan", "Brian"]
 registeredUsers.append("Steve")
 //: [Next](@next)
