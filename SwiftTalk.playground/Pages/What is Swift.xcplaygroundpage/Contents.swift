efbb3b7891d49635a4a33c49eb60090b45239d08
Brian T. Grant <brian.grant@morningstar.com>
2016-06-13 12:58:01 -0500

    Updated 'What is Swift'

--- SwiftTalk.playground/Pages/What is Swift.xcplaygroundpage/Contents.swift
+++ SwiftTalk.playground/Pages/What is Swift.xcplaygroundpage/Contents.swift
@@ -2,20 +2,28 @@
 /*:
  # What is Swift?
  
- * Developed by Apple
+ ## Developed by Apple
  * Swift 1.0 introduced at the 2014 World Wide Developer Conference (WWDC)
- * Open 
+ * Open-sourced in December, 2015
  * Current version:  2.2
- *
  
+ ## Language Design
+ * Goals
+     * Safe
+     * Fast (as a replacement for C-based languages (C, C++, Objective-C))
+     * Expressive
+ * Influenced by C#, CLU, Haskell, Objective-C, Python, Rust, Ruby
+ * Compiled 
+ * Statically typed, strongly typed
+ * Multi-Paradigm
+     * object-oriented
+     * functional
+     * imperative
  
- 
-
- 
- 
- 
- *
- ###
+ ## Supported Platforms
+ * All Apple platforms
+ * Linux (does not use the Objective-C runtime)
+ * Other platforms through 3rd party integration (.NET, Java, Android)
  
  */
 //: [Next](@next)
