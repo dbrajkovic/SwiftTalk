59bc2b66962676ca6cec60371dbe3b6ba801b14b
Brian T. Grant <brian.grant@morningstar.com>
2016-06-13 23:53:36 -0500

    Updated live coding
    Includes "scripted" walkthrough for reference.

---
+++ SwiftTalk.playground/Pages/Inheritence vs Traits.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+/*: 
+ 
+ ## Inheritence vs. Traits
+ * Inheritance lets you define _what your classes are_.
+ * Traits let you define _what classes can do_.
+ 
+ ## Protocols & Protocol Extensions
+ * Protocols Extensions with default behavior ~= Traits
+ * Embrace Composition over Inheritence.
+ 
+ 
+ 
+*/
+
+//: [Next](@next)
---
+++ SwiftTalk.playground/Pages/Live Coding Intro.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+/*:
+# Live Coding!
+
+### Let's see if he can do it, folks!
+ 
+ ![The Super Friends](superfriends.jpeg)
+ 
+*/
+
+//: [Next](@next)
